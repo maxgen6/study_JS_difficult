b7d39b540dcf97f2e2f18996b0f6c45578becd80
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-03-28 19:35:05 +0300

    Переделано оформление, добавлен файл jshint

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-num = 266219;
+let num = 266219;
 let multiply = 1;
 
 num = num.toString();
 
-for (let i = 0; i<num.length; i++){
+for (let i = 0; i < num.length; i++){
     multiply *= num[i];
     console.log(multiply);
 }
 let multiply2 = multiply ** 3; 
 console.log('После возведения в степень: ' + multiply2 );
 multiply2 = multiply2.toString();
-console.log('Первые 2 цифры: ' + multiply2.substr( 0,2 ));
+console.log('Первые 2 цифры: ' + multiply2.substr(0, 2));
