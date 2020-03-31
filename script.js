eb1f6cbfa05b6a55629b6e872ffee1ad73b6dbe7
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-03-31 23:41:05 +0300

    исправленное и закомментированное дз4

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 'use strict';
 
-let line = prompt('Enter the line: ');
-const goOverLine = function(line){
-    if(typeof line === 'string'){
-        line = line.trim();
-        if (line.length() >= 30){
-            line = line.substr(0, 30) + '...';
-        }
-    } else{
-        console.warn('Error');
+let str = prompt('Enter the line: '); // вводим строку
+const goOverLine = function(line){ //задаем функцию goOverLine с аргументом line
+    if(typeof line === 'string'){ //проверяем ли ввели строку
+        line = line.trim(); //убираем пробелы
+        if (line.length >= 30){  //если длинна строки больше 30 символов то
+            line = line.substr(0, 30) + '...'; // то после 30 символа текст не выводим , а выводим многоточие 
+        } 
+        return line; //вызвыаем функцию
     }
-    
-    console.log(line);
-}
-// console.log(goOverLine(line));
-
+};
+console.log(goOverLine(str)); //выводим в консоль
 
 
 
