50bb048d90f18eb43829aac906705dae8e36ec4f
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-04-01 22:15:59 +0300

    недоделанная

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 'use strict';
 
-let str = prompt('Enter the line: '); // вводим строку
-const goOverLine = function(line){ //задаем функцию goOverLine с аргументом line
-    if(typeof line === 'string'){ //проверяем ли ввели строку
-        line = line.trim(); //убираем пробелы
-        if (line.length >= 30){  //если длинна строки больше 30 символов то
-            line = line.substr(0, 30) + '...'; // то после 30 символа текст не выводим , а выводим многоточие 
-        } 
-        return line; //вызвыаем функцию
+let  arr = [];
+
+let searchNumber = function(){
+        for (let i = 0;i < 7;i++){
+        arr[i] = prompt('Enter the number: ');
+
+        if(arr.length === 7){
+            arr[i] = arr[i].split();
+        
+        }
     }
-};
-console.log(goOverLine(str)); //выводим в консоль
-
-
-
-
+    
+}
