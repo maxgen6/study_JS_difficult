885d6919ef31404b38ca2cfd9603c27c9ec7a174
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-04-02 17:47:37 +0300

    не доделанная дз

--- script.js
+++ script.js
@@ -3,13 +3,18 @@
 let  arr = [];
 
 let searchNumber = function(){
-        for (let i = 0;i < 7;i++){
+        for (let i = 0; i < 3; i++){
         arr[i] = prompt('Enter the number: ');
-
-        if(arr.length === 7){
-            arr[i] = arr[i].split();
+        arr[i] = arr[i].toString();
+        console.log(arr[i].indexOf(0));
+       // if(arr[i].length === ){
+        //     if((arr[i].indexOf(2,0) ) && (arr[i].indexOf(4,0) ) ){
+        //         console.log(arr[i]);
+        //     }
+         }  
         
-        }
-    }
     
-}
+    }
+
+
+searchNumber();
